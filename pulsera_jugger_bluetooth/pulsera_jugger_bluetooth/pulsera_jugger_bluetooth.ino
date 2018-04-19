08c3c32e3446eca3de5a90a56c613599de384747
marcel <marcelvimu@outlook.com>
2018-04-19 19:12:07 +0200

    Image added and data read.
    The logo of the team skoll has been added to the code, and the code now can read the string of the jugger protocol and separate it and show on the screen on his correct place.

--- pulsera_jugger_bluetooth/pulsera_jugger_bluetooth/pulsera_jugger_bluetooth.ino
+++ pulsera_jugger_bluetooth/pulsera_jugger_bluetooth/pulsera_jugger_bluetooth.ino
@@ -6,6 +6,13 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+#include "images.h"
+#define DEMO_DURATION 3000
+SSD1306  display(0x3c, 5, 4);
+typedef void (*Demo)(void);
+int demoMode = 0;
+int counter = 1;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -13,11 +20,67 @@
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+
+
+void partido(std::string value) {
+    // Font Demo1
+    // create more fonts at http://oleddisplay.squix.ch/
+    std::string value2=value.substr(0, 1);
+    std::string value3=value.substr(1, 1);
+    char msg[value2.length()+1];
+    char msg2[value3.length()+1];
+    strcpy(msg,value2.c_str()); 
+    strcpy(msg2,value3.c_str()); 
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(8, 0, "Piedras:");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(100, 0, "00");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(8, 14, "Equipo1");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(100, 14, msg);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(8, 28, "Equipo 2:");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(100, 28, msg2);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(8, 42, "Faltas:");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(100, 42, "00");
+    display.display();
+}
+
+ void logo() {//dibuja la imagen introducida
+    // see http://blog.squix.org/2015/05/esp8266-nodemcu-how-to-create-xbm.html
+    // on how to create xbm files
+    display.clear();
+    display.drawXbm(33, 0, skoll_width, skoll_height, skoll_bits);
+    display.display();
+}
+
+void pantalla(std::string value) {
+    // Font Demo1
+    // create more fonts at http://oleddisplay.squix.ch/
+
+    char msg[value.length()+1];
+    strcpy(msg,value.c_str()); //copia datos directamente de un string
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+     display.setFont(ArialMT_Plain_24);
+    display.drawStringMaxWidth(0, 0,128, msg);//limita el numero de caracteres en la linea y escribe en la siguiente si no entra
+    Serial.println(msg);
+    display.display();
+    
+}
+
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
-
-      if (value.length() > 0) {
+ 
+      if (value.length() > 0) {      
+ 
         Serial.println("*********");
         Serial.print("New value: ");
         for (int i = 0; i < value.length(); i++)
@@ -25,11 +88,27 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 
         Serial.println();
         Serial.println("*********");
+        partido(value);
+        
+       
       }
     }
 };
+
 void setup() {
   Serial.begin(115200);
+// Initialising the UI will init the display too.
+  display.init();
+  display.clear();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  // write the buffer to the display
+  
+  display.display();
+ 
+  logo();
+  
+  
   Serial.println("Starting BLE work!");
 
   BLEDevice::init("JuggerBand");
@@ -52,6 +131,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
   delay(2000);
 }
